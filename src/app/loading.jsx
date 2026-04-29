74cfa8ca9c4ad63729d840425f5c9508fbdc598b
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-04-29 23:27:02 +0600

    added loading spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <h2 className='text-4xl text-red-500'>Loading</h2>
+        </div>
+    );
+};
+
+export default loading;
