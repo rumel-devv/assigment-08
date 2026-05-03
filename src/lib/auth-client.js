004d03b5aab8a778db4c100cdf3f307f05072f22
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-05-03 23:32:56 +0600

    fixed url problem

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL:"http://localhost:3000"
+    baseURL:"https://assigment-08-chi.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
